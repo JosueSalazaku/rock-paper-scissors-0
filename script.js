b8ac34331b792a7c0071413a8b354189f57eb80d
Josué Salazaku <josuesalazaku@gmail.com>
2023-11-08 22:34:03 +0100

    Add play function

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 const rockBtn = document.querySelector(".rock");
 const paperkBtn = document.querySelector(".paper");
 const scissorkBtn = document.querySelector(".scissor");
+const playBtn = document.querySelector(".computer");
+const allBtn = document.querySelector("#buttons");
+const choices = ["rock", "paper", "scissors"];
+
+let playerChoice;
+let computerChoice;
+
+function play() {
+  const choices = ["rock", "paper", "scissors"];
+  computerChoice = choices[Math.floor(Math.random() * 3)];
+
+  if (playerChoice) {
+    if (playerChoice === computerChoice) {
+      console.log("It's a draw!");
+    }
+  }
+}
