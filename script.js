434217747fbc221d92f6b8d8ccb9580802ec800b
Josué Salazaku <josuesalazaku@gmail.com>
2023-11-09 11:07:39 +0100

    Add events to buttons

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const rockBtn = document.querySelector(".rock");
-const paperkBtn = document.querySelector(".paper");
-const scissorkBtn = document.querySelector(".scissor");
+const paperBtn = document.querySelector(".paper");
+const scissorBtn = document.querySelector(".scissor");
 const playBtn = document.querySelector(".computer");
 const allBtn = document.querySelector("#buttons");
 const choices = ["rock", "paper", "scissors"];
@@ -19,6 +19,36 @@ function play() {
   if (playerChoice) {
     if (playerChoice === computerChoice) {
       console.log("It's a draw!");
+    } else if (
+      (playerChoice === "rock" && computerChoice === "scissor") ||
+      (playerChoice === "paper" && computerChoice === "rock") ||
+      (playerChoice === "scissor" && computerChoice === "paper")
+    ) {
+      console.log("Player wins!");
+    } else {
+      console.log("Player Lost!");
     }
+
+    playerChoice = null;
+    computerChoice = null;
+  } else {
+    console.log("Please make a choice first.");
   }
 }
+
+rockBtn.addEventListener("click", function () {
+  playerChoice = "rock";
+  console.log("rock");
+});
+
+paperBtn.addEventListener("click", function () {
+  playerChoice = "paper";
+  console.log("paper");
+});
+
+scissorBtn.addEventListener("click", function () {
+  playerChoice = "scissor";
+  console.log("scissor");
+});
+
+playBtn.addEventListener("click", play);
